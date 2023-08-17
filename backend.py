f9157ac63984788d36c92373f236c9830065468c
Aaron Balke <aabalke33@gmail.com>
2023-08-16 20:08:24 -0600

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,24 +1,55 @@
 import time
-import streamlit as st
 import torch
+# from auto_gptq import AutoGPTQForCausalLM
+from huggingface_hub import hf_hub_download
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.llms import HuggingFacePipeline
-from constants import EMBEDDING_MODEL_NAME, db_all, db_frankl, db_inst
+from langchain.llms import HuggingFacePipeline, LlamaCpp
+from langchain.prompts import PromptTemplate
+
 from langchain.vectorstores import Chroma
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     GenerationConfig,
-    LlamaForCausalLM,
-    LlamaTokenizer,
     pipeline,
 )
 
+from constants import CHROMA_SETTINGS, EMBEDDING_MODEL_NAME, PERSIST_DIRECTORY
 
-@st.cache_resource(show_spinner=False)
-def load_model(device_type, model_id):
-    if device_type.lower() == "cuda":  # cuda
+
+def load_model(model_id, model_basename=None):
+
+    if model_basename is not None:
+        if ".ggml" in model_basename:
+            model_path = hf_hub_download(
+                repo_id=model_id,
+                filename=model_basename
+            )
+            max_ctx_size = 2048
+            kwargs = {"model_path": model_path,
+                      "n_ctx": max_ctx_size,
+                      "max_tokens": max_ctx_size,
+                      "n_gpu_layers": 1000,
+                      "n_batch": max_ctx_size}
+            return LlamaCpp(**kwargs)
+
+        # else:
+        #     if ".safetensors" in model_basename:
+        #         model_basename = model_basename.replace(".safetensors", "")
+
+        #     tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+
+        #     model = AutoGPTQForCausalLM.from_quantized(
+        #         model_id,
+        #         model_basename=model_basename,
+        #         use_safetensors=True,
+        #         trust_remote_code=True,
+        #         device="cuda:0",
+        #         use_triton=False,
+        #         quantize_config=None,
+        #     )
+    else:
         tokenizer = AutoTokenizer.from_pretrained(model_id)
 
         model = AutoModelForCausalLM.from_pretrained(
@@ -29,9 +60,6 @@ def load_model(device_type, model_id):
             trust_remote_code=True,
         )
         model.tie_weights()
-    else:  # cpu
-        tokenizer = LlamaTokenizer.from_pretrained(model_id)
-        model = LlamaForCausalLM.from_pretrained(model_id)
 
     generation_config = GenerationConfig.from_pretrained(model_id)
 
@@ -39,84 +67,56 @@ def load_model(device_type, model_id):
         "text-generation",
         model=model,
         tokenizer=tokenizer,
+        min_length=512,
         max_length=2048,
-        temperature=0.1,
+        temperature=0,
         top_p=0.95,
-        repetition_penalty=1.5,
+        repetition_penalty=1.15,
         generation_config=generation_config,
     )
 
-    local_llm = HuggingFacePipeline(pipeline=pipe)
-
-    return local_llm
+    return HuggingFacePipeline(pipeline=pipe)
 
 
-@st.cache_data(persist=True, show_spinner=False)
-def get_embeddings():
-    if torch.cuda.is_available():
-        device_type = "cuda"
-    else:
-        device_type = "cpu"
+def load_qa():
 
     embeddings = HuggingFaceInstructEmbeddings(
         model_name=EMBEDDING_MODEL_NAME,
-        model_kwargs={"device": device_type}
+        model_kwargs={"device": "cuda"}
     )
-
-    return embeddings
-
-
-def get_llm():
-    if torch.cuda.is_available():
-        device_type = "cuda"
-    else:
-        device_type = "cpu"
-
-    model_id = "psmathur/orca_mini_3b"
-
-    llm = load_model(
-        device_type,
-        model_id=model_id
-    )
-    return llm
-
-
-def load_qa(db_option):
-    db_instance = db_inst
-
-    match db_option:
-        case "All":
-            db_instance = db_all
-            print("loading db_all")
-        case "Frankl's Works":
-            db_instance = db_frankl
-            print("loading db_frankl")
-        case "Journal of Search for Meaning":
-            db_instance = db_inst
-            print("loading db_inst")
-
-    load_start = time.time()
-
-    embeddings = get_embeddings()
-
     db = Chroma(
-        persist_directory=db_instance.get_directory(),
+        persist_directory=PERSIST_DIRECTORY,
         embedding_function=embeddings,
-        client_settings=db_instance.get_chroma_settings(),
+        client_settings=CHROMA_SETTINGS,
     )
-
     retriever = db.as_retriever()
 
-    llm = get_llm()
+    model_id = "psmathur/orca_mini_3b"
+    model_basename = None
 
+    # model_id = "TheBloke/vicuna-7B-1.1-HF"
+    # model_basename = None
+
+    template = """You are an AI assistant for answering questions about logotherapy. You are given the following
+    extracted parts of a annual academic journal. Provide a very detailed comprehensive academic answer. If you don't
+    know the answer, just say "I'm not sure." Don't try to make up an answer. If the question is not about the
+    psychotherapy and not directly in the given context, politely inform them that you are tuned to only answer
+    questions about logotherapy. Question: {question} ========= {context} ========= Answer:"""
+
+    prompt = PromptTemplate(
+        input_variables=["context", "question"],
+        template=template
+    )
+    llm = load_model(
+        model_id=model_id,
+        model_basename=model_basename
+    )
     qa = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         retriever=retriever,
         return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
     )
-
-    load_end = time.time()
-    print(f"\n> Completed Initial Load (took {round(load_end - load_start, 2)} s.)")
 
     return qa
